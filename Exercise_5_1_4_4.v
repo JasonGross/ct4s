935ef6fc759770f0964161af859d7fa13e6bce02
Jason Gross <jgross@mit.edu>
2013-04-26 09:47:35 -0400

    Add part of pset 10

---
+++ Exercise_5_1_4_4.v
@@ -0,0 +1,178 @@
+(* printing '*' %\ensuremath{\times}% #&times;# *)
+(* printing * %\ensuremath{\times}% #&times;# *)
+(** printing '×' %\ensuremath{\times}% #&times;# *)
+(** printing × %\ensuremath{\times}% #&times;# *)
+(** printing ★ %\ensuremath{\star}% #&#9733;# *)
+(** printing "★" %\ensuremath{\star}% #&#9733;# *)
+(** printing '★' %\ensuremath{\star}% #&#9733;# *)
+(** printing '⊔' %\ensuremath{\sqcup}% #&#x2294;# *)
+(** printing ⊔ %\ensuremath{\sqcup}% #&#x2294;# *)
+(** printing ∪ %\ensuremath{\cup}% #&cup;# *)
+(** printing '∪' %\ensuremath{\cup}% #&cup;# *)
+(** printing ∩ %\ensuremath{\cap}% #&cap;# *)
+(** printing '∩' %\ensuremath{\cap}% #&cap;# *)
+(** printing 'π' %\ensuremath{\pi}% #&pi;# *)
+(** printing π %\ensuremath{\pi}% #&pi;# *)
+(** printing 'Σ' %\ensuremath{\Sigma}% #&Sigma;# *)
+(** printing Σ %\ensuremath{\Sigma}% #&Sigma;# *)
+(** printing ⁰ %\ensuremath{^0}% #<sup>0</sup># *)
+(** printing ¹ %\ensuremath{^1}% #<sup>1</sup># *)
+(** printing ² %\ensuremath{^2}% #<sup>2</sup># *)
+(** printing ³ %\ensuremath{^3}% #<sup>3</sup># *)
+(** printing ⁴ %\ensuremath{^4}% #<sup>4</sup># *)
+(** printing ⁵ %\ensuremath{^5}% #<sup>5</sup># *)
+(** printing ⁶ %\ensuremath{^6}% #<sup>6</sup># *)
+(** printing ⁷ %\ensuremath{^7}% #<sup>7</sup># *)
+(** printing ⁸ %\ensuremath{^9}% #<sup>9</sup># *)
+(** printing ⁹ %\ensuremath{^9}% #<sup>9</sup># *)
+(** printing ⁱ %\ensuremath{^i}% #<sup>i</sup># *)
+(** printing ⁺ %\ensuremath{^+}% #<sup>+</sup># *)
+(** printing ⁻ %\ensuremath{^-}% #<sup>-</sup># *)
+(** printing ⁼ %\ensuremath{^=}% #<sup>=</sup># *)
+(** printing ⁽ %\ensuremath{^(}% #<sup>(</sup># *)
+(** printing ⁾ %\ensuremath{^)}% #<sup>)</sup># *)
+(** printing ⁿ %\ensuremath{^n}% #<sup>n</sup># *)
+(** printing ₀ %\ensuremath{_0}% #<sub>0</sub># *)
+(** printing ₁ %\ensuremath{_1}% #<sub>1</sub># *)
+(** printing ₂ %\ensuremath{_2}% #<sub>2</sub># *)
+(** printing ₃ %\ensuremath{_3}% #<sub>3</sub># *)
+(** printing ₄ %\ensuremath{_4}% #<sub>4</sub># *)
+(** printing ₅ %\ensuremath{_5}% #<sub>5</sub># *)
+(** printing ₆ %\ensuremath{_6}% #<sub>6</sub># *)
+(** printing ₇ %\ensuremath{_7}% #<sub>7</sub># *)
+(** printing ₈ %\ensuremath{_9}% #<sub>9</sub># *)
+(** printing ₉ %\ensuremath{_9}% #<sub>9</sub># *)
+(** printing ₊ %\ensuremath{_+}% #<sub>+</sub># *)
+(** printing ₋ %\ensuremath{_-}% #<sub>-</sub># *)
+(** printing ₌ %\ensuremath{_=}% #<sub>=</sub># *)
+(** printing ₍ %\ensuremath{_(}% #<sub>(</sub># *)
+(** printing ₎ %\ensuremath{_)}% #<sub>)</sub># *)
+(** printing ₐ %\ensuremath{_a}% #<sub>a</sub># *)
+(** printing ₑ %\ensuremath{_e}% #<sub>e</sub># *)
+(** printing ₒ %\ensuremath{_o}% #<sub>o</sub># *)
+(** printing ₓ %\ensuremath{_x}% #<sub>x</sub># *)
+(** printing ᵒᵖ %\ensuremath{^{\text{op}}}% #<sup>op</sup># *)
+(** printing π₁ %\ensuremath{\pi_1}% #&pi;<sub>1</sub># *)
+(** printing π₂ %\ensuremath{\pi_2}% #&pi;<sub>2</sub># *)
+(** printing 'π₁' %\ensuremath{\pi_1}% #&pi;<sub>1</sub># *)
+(** printing 'π₂' %\ensuremath{\pi_2}% #&pi;<sub>2</sub># *)
+(** printing f₀ %\ensuremath{f_0}% #f<sub>0</sub># *)
+(** printing f₀) %\ensuremath{f_0})% #f<sub>0</sub>)# *)
+(** printing f₁ %\ensuremath{f_1}% #f<sub>1</sub># *)
+(** printing f₁) %\ensuremath{f_1})% #f<sub>1</sub>)# *)
+(** printing ≅ %\ensuremath{\cong}% #&cong;# *)
+(** printing ≃ %\ensuremath{\simeq}% #&#x2243;# *)
+(** printing ≄ %\ensuremath{\not\simeq}% #&#9772;# *)
+(** printing λ %\ensuremath{\lambda}% #&lambda;# *)
+(** printing 'o' %\ensuremath{\circ}% #&#x25cb;# *)
+(** printing o %\ensuremath{\circ}% #&#x25cb;# *)
+(** printing i ⁻¹ %\ensuremath{i^{-1}}% #i<sup>-1</sup># *)
+(** printing 'i ⁻¹' %\ensuremath{i^{-1}}% #i<sup>-1</sup># *)
+(** printing "i ⁻¹" %\ensuremath{i^{-1}}% #i<sup>-1</sup># *)
+(** printing ⁻¹ %\ensuremath{^{-1}}% #<sup>-1</sup># *)
+(** printing '⁻¹' %\ensuremath{^{-1}}% #<sup>-1</sup># *)
+(** printing '⁻' %\ensuremath{^{-}}% #<sup>-</sup># *)
+(** printing '¹' %\ensuremath{^{1}}% #<sup>1</sup># *)
+(** printing ⁻ %\ensuremath{^{-}}% #<sup>-</sup># *)
+(** printing ¹ %\ensuremath{^{1}}% #<sup>1</sup># *)
+(** printing :> %:\ensuremath{>}% #:># *)
+(** printing ':>' %:\ensuremath{>}% #:># *)
+(** printing _1_ %\ensuremath{\text{\underline{1}}}% #<u>1</u># *)
+(** printing '_1_' %\ensuremath{\text{\underline{1}}}% #<u>1</u># *)
+(** printing _2_ %\ensuremath{\text{\underline{2}}}% #<u>2</u># *)
+(** printing '_2_' %\ensuremath{\text{\underline{2}}}% #<u>2</u># *)
+(** printing ℝ %\ensuremath{\mathbb{R}}% #&#x211d;# *)
+(** printing ℝ³ %\ensuremath{\mathbb{R}^3}% #&#x211d;<sup>3</sup># *)
+(** printing ℕ %\ensuremath{\mathbb{N}}% #&#x2115;# *)
+(** printing ℤ %\ensuremath{\mathbb{Z}}% #&#x2124;# *)
+(** printing ℙ %\ensuremath{\mathbb{P}}% #&#x2119;# *)
+(** printing ← %\ensuremath{\leftarrow}% #&larr;# *)
+(** printing ↑ %\ensuremath{\uparrow}% #&uarr;# *)
+(** printing → %\ensuremath{\rightarrow}% #&rarr;# *)
+(** printing ↓ %\ensuremath{\downarrow}% #&darr;# *)
+(** printing ↔ %\ensuremath{\leftrightarrow}% #&harr;# *)
+(** printing ↕ %\ensuremath{\updownarrow}% #&#x2195# *)
+(** printing ↖ %\ensuremath{\nwarrow}% #&#x2196# *)
+(** printing ↗ %\ensuremath{\nearrow}% #&#x2197# *)
+(** printing ↘ %\ensuremath{\searrow}% #&#x2199# *)
+(** printing ↙ %\ensuremath{\swarrow}% #&#x2199# *)
+(** printing ⇉ %\ensuremath{\rightrightarrows} #&#x21c9# *)
+(** printing ↦ %\ensuremath{\mapsto}% #&#21A6# *)
+(** printing ↪ %\ensuremath{\hookrightarrow}% #&rarrhk;# *)
+(** printing ⊆ %\ensuremath{\subseteq}% #&sube;# *)
+(** printing ⊂ %\ensuremath{\subset}% #&sub;# *)
+(** printing ∈ %\ensuremath{\in}% #&isin;# *)
+(** printing ∅ %\ensuremath{\emptyset}% #&empty;# *)
+(** printing ▻ %\ensuremath{\triangleright}% #&#x25BB;# *)
+(** printing ◅ %\ensuremath{\triangleleft}% #&#x25C5;# *)
+(** printing ≤ %\ensuremath{\le}% #&le;# *)
+(** printing ≤) %\ensuremath{\le)}% #&le;)# *)
+(** printing ≰ %\ensuremath{\not\le}% #&#x2270;# *)
+(** printing ≱ %\ensuremath{\not\ge}% #&#x2271;# *)
+(** printing ≼ %\ensuremath{\preceq}% #&#x227c;# *)
+(** printing ≽ %\ensuremath{\succeq}% #&#x227d;# *)
+(** printing ∧ %\ensuremath{\wedge}% #&and;# *)
+(** printing ∨ %\ensuremath{\vee}% #&or;# *)
+(** printing ¬ %\ensuremath{\neg}% #&not;# *)
+(** printing ¬( %\ensuremath{\neg}(% #&not;(# *) (* ))) *)
+(* must \usepackage{mathabx} in LaTeX *)
+(** printing ↷ %\ensuremath{\lefttorightarrow}% #<div style="display:inline-block; transform:rotate(90deg);-o-transform:rotate(90deg);-mod-transform:rotate(90deg);-webkit-transform:rotate(90deg);">&#x21ba;</div># *)
+
+Require Import Utf8.
+Require Import JMeq ProofIrrelevance.
+Require Import Graph Category Isomorphism.
+Require Import Common Notations.
+
+Set Implicit Arguments.
+
+Generalizable All Variables.
+
+(** ------------------------------------------------------------------------ *)
+
+(** * Exercise 5.1.4.4 *)
+Module Exercise_5_1_4_4.
+  (** ** Problem *)
+  (** Let [C = (G, ≃)] be a schema. A leaf table is an object [c : Ob
+      C] with no outgoing arrows. Write this condition mathematically
+      in three different languages: that of graphs (using symbols [V], [A],
+      [src], [tgt]), that of categories (using [Hom_C], etc.), and that of
+      tables (in terms of columns, tables, rows, etc.).
+
+      In the language of categories, is there a difference between a
+      terminal object and a leaf table? *)
+  (** ** Solution *)
+  (** (a)
+
+      - Graphs: A leaf table is a vertex [v : V] such that no edge has
+        as source [v]. (see below for symbolic version)
+
+      - Categories: A leaf table is an object [c : C] such that there
+        is no non-identity morphism from [c].
+
+      - Tables: A leaf table is a table with exactly one column.
+
+      (b) A leaf table has no outgoing arrows except the identity
+          arrow.  In the discrete schema, every table is a leaf table.
+          A terminal object has exactly one incoming arrow for every
+          other object.  They're not the same. *)
+
+  Section GraphLeafTable.
+    Variable G : Graph'.
+    Let V := Vertex' G.
+    Let A := Arrow' G.
+    Let src := Graph'Source G.
+    Let tgt := Graph'Target G.
+
+    Definition GraphLeafTable (v : V) := forall a : A, src a <> v.
+  End GraphLeafTable.
+
+  Section CategoryLeafTable.
+    Let Ob {objC} (C : @Category objC) := Object C.
+    Let Hom {objC} (C : @Category objC) := Morphism C.
+    Context `(C : @Category objC).
+
+    Definition CategoryLeafTable (c : Ob C) :=
+      forall d : Ob C, Hom C c d ≅ (c = d).
+  End CategoryLeafTable.
+End Exercise_5_1_4_4.
+(** ------------------------------------------------------------------------ *)
---
+++ Exercise_5_1_4_5.v
@@ -0,0 +1,219 @@
+(* printing '*' %\ensuremath{\times}% #&times;# *)
+(* printing * %\ensuremath{\times}% #&times;# *)
+(** printing '×' %\ensuremath{\times}% #&times;# *)
+(** printing × %\ensuremath{\times}% #&times;# *)
+(** printing ★ %\ensuremath{\star}% #&#9733;# *)
+(** printing "★" %\ensuremath{\star}% #&#9733;# *)
+(** printing '★' %\ensuremath{\star}% #&#9733;# *)
+(** printing '⊔' %\ensuremath{\sqcup}% #&#x2294;# *)
+(** printing ⊔ %\ensuremath{\sqcup}% #&#x2294;# *)
+(** printing ∪ %\ensuremath{\cup}% #&cup;# *)
+(** printing '∪' %\ensuremath{\cup}% #&cup;# *)
+(** printing ∩ %\ensuremath{\cap}% #&cap;# *)
+(** printing '∩' %\ensuremath{\cap}% #&cap;# *)
+(** printing 'π' %\ensuremath{\pi}% #&pi;# *)
+(** printing π %\ensuremath{\pi}% #&pi;# *)
+(** printing 'Σ' %\ensuremath{\Sigma}% #&Sigma;# *)
+(** printing Σ %\ensuremath{\Sigma}% #&Sigma;# *)
+(** printing ⁰ %\ensuremath{^0}% #<sup>0</sup># *)
+(** printing ¹ %\ensuremath{^1}% #<sup>1</sup># *)
+(** printing ² %\ensuremath{^2}% #<sup>2</sup># *)
+(** printing ³ %\ensuremath{^3}% #<sup>3</sup># *)
+(** printing ⁴ %\ensuremath{^4}% #<sup>4</sup># *)
+(** printing ⁵ %\ensuremath{^5}% #<sup>5</sup># *)
+(** printing ⁶ %\ensuremath{^6}% #<sup>6</sup># *)
+(** printing ⁷ %\ensuremath{^7}% #<sup>7</sup># *)
+(** printing ⁸ %\ensuremath{^9}% #<sup>9</sup># *)
+(** printing ⁹ %\ensuremath{^9}% #<sup>9</sup># *)
+(** printing ⁱ %\ensuremath{^i}% #<sup>i</sup># *)
+(** printing ⁺ %\ensuremath{^+}% #<sup>+</sup># *)
+(** printing ⁻ %\ensuremath{^-}% #<sup>-</sup># *)
+(** printing ⁼ %\ensuremath{^=}% #<sup>=</sup># *)
+(** printing ⁽ %\ensuremath{^(}% #<sup>(</sup># *)
+(** printing ⁾ %\ensuremath{^)}% #<sup>)</sup># *)
+(** printing ⁿ %\ensuremath{^n}% #<sup>n</sup># *)
+(** printing ₀ %\ensuremath{_0}% #<sub>0</sub># *)
+(** printing ₁ %\ensuremath{_1}% #<sub>1</sub># *)
+(** printing ₂ %\ensuremath{_2}% #<sub>2</sub># *)
+(** printing ₃ %\ensuremath{_3}% #<sub>3</sub># *)
+(** printing ₄ %\ensuremath{_4}% #<sub>4</sub># *)
+(** printing ₅ %\ensuremath{_5}% #<sub>5</sub># *)
+(** printing ₆ %\ensuremath{_6}% #<sub>6</sub># *)
+(** printing ₇ %\ensuremath{_7}% #<sub>7</sub># *)
+(** printing ₈ %\ensuremath{_9}% #<sub>9</sub># *)
+(** printing ₉ %\ensuremath{_9}% #<sub>9</sub># *)
+(** printing ₊ %\ensuremath{_+}% #<sub>+</sub># *)
+(** printing ₋ %\ensuremath{_-}% #<sub>-</sub># *)
+(** printing ₌ %\ensuremath{_=}% #<sub>=</sub># *)
+(** printing ₍ %\ensuremath{_(}% #<sub>(</sub># *)
+(** printing ₎ %\ensuremath{_)}% #<sub>)</sub># *)
+(** printing ₐ %\ensuremath{_a}% #<sub>a</sub># *)
+(** printing ₑ %\ensuremath{_e}% #<sub>e</sub># *)
+(** printing ₒ %\ensuremath{_o}% #<sub>o</sub># *)
+(** printing ₓ %\ensuremath{_x}% #<sub>x</sub># *)
+(** printing ᵒᵖ %\ensuremath{^{\text{op}}}% #<sup>op</sup># *)
+(** printing π₁ %\ensuremath{\pi_1}% #&pi;<sub>1</sub># *)
+(** printing π₂ %\ensuremath{\pi_2}% #&pi;<sub>2</sub># *)
+(** printing 'π₁' %\ensuremath{\pi_1}% #&pi;<sub>1</sub># *)
+(** printing 'π₂' %\ensuremath{\pi_2}% #&pi;<sub>2</sub># *)
+(** printing f₀ %\ensuremath{f_0}% #f<sub>0</sub># *)
+(** printing f₀) %\ensuremath{f_0})% #f<sub>0</sub>)# *)
+(** printing f₁ %\ensuremath{f_1}% #f<sub>1</sub># *)
+(** printing f₁) %\ensuremath{f_1})% #f<sub>1</sub>)# *)
+(** printing ≅ %\ensuremath{\cong}% #&cong;# *)
+(** printing ≃ %\ensuremath{\simeq}% #&#x2243;# *)
+(** printing ≄ %\ensuremath{\not\simeq}% #&#9772;# *)
+(** printing λ %\ensuremath{\lambda}% #&lambda;# *)
+(** printing 'o' %\ensuremath{\circ}% #&#x25cb;# *)
+(** printing o %\ensuremath{\circ}% #&#x25cb;# *)
+(** printing i ⁻¹ %\ensuremath{i^{-1}}% #i<sup>-1</sup># *)
+(** printing 'i ⁻¹' %\ensuremath{i^{-1}}% #i<sup>-1</sup># *)
+(** printing "i ⁻¹" %\ensuremath{i^{-1}}% #i<sup>-1</sup># *)
+(** printing ⁻¹ %\ensuremath{^{-1}}% #<sup>-1</sup># *)
+(** printing '⁻¹' %\ensuremath{^{-1}}% #<sup>-1</sup># *)
+(** printing '⁻' %\ensuremath{^{-}}% #<sup>-</sup># *)
+(** printing '¹' %\ensuremath{^{1}}% #<sup>1</sup># *)
+(** printing ⁻ %\ensuremath{^{-}}% #<sup>-</sup># *)
+(** printing ¹ %\ensuremath{^{1}}% #<sup>1</sup># *)
+(** printing :> %:\ensuremath{>}% #:># *)
+(** printing ':>' %:\ensuremath{>}% #:># *)
+(** printing _1_ %\ensuremath{\text{\underline{1}}}% #<u>1</u># *)
+(** printing '_1_' %\ensuremath{\text{\underline{1}}}% #<u>1</u># *)
+(** printing _2_ %\ensuremath{\text{\underline{2}}}% #<u>2</u># *)
+(** printing '_2_' %\ensuremath{\text{\underline{2}}}% #<u>2</u># *)
+(** printing ℝ %\ensuremath{\mathbb{R}}% #&#x211d;# *)
+(** printing ℝ³ %\ensuremath{\mathbb{R}^3}% #&#x211d;<sup>3</sup># *)
+(** printing ℕ %\ensuremath{\mathbb{N}}% #&#x2115;# *)
+(** printing ℤ %\ensuremath{\mathbb{Z}}% #&#x2124;# *)
+(** printing ℙ %\ensuremath{\mathbb{P}}% #&#x2119;# *)
+(** printing ← %\ensuremath{\leftarrow}% #&larr;# *)
+(** printing ↑ %\ensuremath{\uparrow}% #&uarr;# *)
+(** printing → %\ensuremath{\rightarrow}% #&rarr;# *)
+(** printing ↓ %\ensuremath{\downarrow}% #&darr;# *)
+(** printing ↔ %\ensuremath{\leftrightarrow}% #&harr;# *)
+(** printing ↕ %\ensuremath{\updownarrow}% #&#x2195# *)
+(** printing ↖ %\ensuremath{\nwarrow}% #&#x2196# *)
+(** printing ↗ %\ensuremath{\nearrow}% #&#x2197# *)
+(** printing ↘ %\ensuremath{\searrow}% #&#x2199# *)
+(** printing ↙ %\ensuremath{\swarrow}% #&#x2199# *)
+(** printing ⇉ %\ensuremath{\rightrightarrows} #&#x21c9# *)
+(** printing |-> %\ensuremath{\mapsto}% #&#21A6# *)
+(** printing ↦ %\ensuremath{\mapsto}% #&#21A6# *)
+(** printing ↪ %\ensuremath{\hookrightarrow}% #&rarrhk;# *)
+(** printing ⊆ %\ensuremath{\subseteq}% #&sube;# *)
+(** printing ⊂ %\ensuremath{\subset}% #&sub;# *)
+(** printing ∈ %\ensuremath{\in}% #&isin;# *)
+(** printing ∅ %\ensuremath{\emptyset}% #&empty;# *)
+(** printing ▻ %\ensuremath{\triangleright}% #&#x25BB;# *)
+(** printing ◅ %\ensuremath{\triangleleft}% #&#x25C5;# *)
+(** printing ≤ %\ensuremath{\le}% #&le;# *)
+(** printing ≤) %\ensuremath{\le)}% #&le;)# *)
+(** printing ≰ %\ensuremath{\not\le}% #&#x2270;# *)
+(** printing ≱ %\ensuremath{\not\ge}% #&#x2271;# *)
+(** printing ≼ %\ensuremath{\preceq}% #&#x227c;# *)
+(** printing ≽ %\ensuremath{\succeq}% #&#x227d;# *)
+(** printing ∧ %\ensuremath{\wedge}% #&and;# *)
+(** printing ∨ %\ensuremath{\vee}% #&or;# *)
+(** printing ¬ %\ensuremath{\neg}% #&not;# *)
+(** printing ¬( %\ensuremath{\neg}(% #&not;(# *) (* ))) *)
+(* must \usepackage{mathabx} in LaTeX *)
+(** printing ↷ %\ensuremath{\lefttorightarrow}% #<div style="display:inline-block; transform:rotate(90deg);-o-transform:rotate(90deg);-mod-transform:rotate(90deg);-webkit-transform:rotate(90deg);">&#x21ba;</div># *)
+
+Require Import Utf8.
+Require Import Category.
+Require Import Common Notations.
+
+Set Implicit Arguments.
+
+Generalizable All Variables.
+
+(** ------------------------------------------------------------------------ *)
+
+(** * Exercise 5.1.4.5 *)
+Module Exercise_5_1_4_5.
+  (** ** Problem *)
+  (** Consider the schemas
+
+<<
+      [1] = 0 -- f --> 1
+      and
+      [2] = 0 -- g --> 1 -- h --> 2
+>>
+
+      and the functor [F : [1] -> [2]] given by sending [0 |-> 0] and
+      [1 |-> 2].
+
+      (a) How many possibilities are there for [F(f)]?
+
+      (b) Now suppose [I : [2] -> Set] is given by the following
+          tables.
+
+<<
+                0
+      -----------------------
+      ID        | g
+      -----------------------
+      Am        | To be verb
+      Baltimore | Place
+      Carla     | Person
+      Develop   | Action verb
+      Edward    | Person
+      Foolish   | Adjective
+      Green     | Adjective
+
+
+                  1
+      -----------------------
+      ID          | h
+      -----------------------
+      Action verb | Verb
+      Adjective   | Adjective
+      Place       | Noun
+      Person      | Noun
+      To be verb  | Verb
+
+
+           2
+      ---------
+      ID
+      ---------
+      Adjective
+      Noun
+      Verb
+
+>>
+
+
+      Write out the two tables associated to the [[1]]-instance
+      [∆_F(I) : [1] -> Set].  *)
+  (** ** Solution *)
+  (** (a) 1: [h o g]
+
+      (b)
+
+<<
+                0
+      -----------------------
+      ID        | h o g
+      -----------------------
+      Am        | Verb
+      Baltimore | Noun
+      Carla     | Noun
+      Develop   | Verb
+      Edward    | Noun
+      Foolish   | Adjective
+      Green     | Adjective
+
+
+           1
+      ---------
+      ID
+      ---------
+      Adjective
+      Noun
+      Verb
+>>
+
+   *)
+End Exercise_5_1_4_5.
+(** ------------------------------------------------------------------------ *)
